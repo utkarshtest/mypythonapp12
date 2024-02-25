5fff06288f727e957661f71ff702ab49302d70b5
utkarshtest <161074333+utkarshtest@users.noreply.github.com>
2024-02-25 17:44:25 +0530

    Update parser.py

--- parser.py
+++ parser.py
@@ -18,15 +18,10 @@ with open('out.txt', 'r') as f:
             comment_url = f"https://api.github.com/repos/{repo}/issues/{pr_number}/comments"
             comment_data = {"body": "Blocked - High Vulnerability Found"}
             headers = {"Authorization": f"Bearer {token}", "Content-Type": "application/json"}
-
             response = requests.post(comment_url, json=comment_data, headers=headers)
             print(response.content)
-            # Set the pull request status to failure
-            # status_url = f"https://api.github.com/repos/{repo}/statuses/{os.environ.get('GITHUB_SHA')}"
-            # status_data = {"state": "failure", "context": "security-check", "description": "Blocked due to high vulnerability"}
-
-            # response = requests.post(status_url, json=status_data, headers=headers)
+           
             # print(response.status_code)
-            sys.exit(0)
+            sys.exit(2)
 
 sys.exit(0)
